a6a457c09b5cb7e9f7c25b1db3c6548692b90066
EvertonRobertoAuler <evertonrobertoauler@gmail.com.br>
2013-08-13 00:04:04 -0300

    fazendo ajustes nas telas e adicionando palestrantes

--- evento/admin.py
+++ evento/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from mezzanine.pages.admin import PageAdmin
-from .models import ProgramacaoPage, Programacao
+from .models import ProgramacaoPage, Programacao, Palestrante, Participante
 
 class ProgramacaoInline(admin.TabularInline):
     model = Programacao
@@ -9,3 +9,5 @@ class ProgramacaoAdmin(PageAdmin):
     inlines = (ProgramacaoInline,)
 
 admin.site.register(ProgramacaoPage, ProgramacaoAdmin)
+admin.site.register(Palestrante)
+admin.site.register(Participante)
